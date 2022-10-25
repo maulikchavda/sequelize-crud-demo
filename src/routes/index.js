8236cbeaf774c6424dac7b7398018f5de58e39a4
maulikchavda <maulikchavda1311@gmail.com>
2022-10-25 18:19:13 +0530

    Update route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
 const crudRoute = require('./crud')
-router.use('/projects', crudRoute)
+router.use('/api/projects', crudRoute)
 
 module.exports = router
