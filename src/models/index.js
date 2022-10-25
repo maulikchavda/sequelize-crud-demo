fc14412f45ea84fdb7ce51bd1d791049d2ada355
maulikchavda <maulikchavda1311@gmail.com>
2022-10-25 19:10:17 +0530

    >> Rollback model index

--- src/models/index.js
+++ src/models/index.js
@@ -3,17 +3,23 @@
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
-// const process = require('process')
+const process = require('process')
 const basename = path.basename(__filename)
-// const env = process.env.NODE_ENV || 'development'
-// const config = require(__dirname + '/../config/config.js')[env]
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.js')[env]
 const db = {}
 
 let sequelize
-  sequelize = new Sequelize('cruddemo', 'root', 'Maulik@1311', {
-    host: 'localhost',
-    dialect: 'mysql',
-  })
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+  )
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
